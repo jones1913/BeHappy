59f36c35bd3ffa1a5df6323b35f4dcd31c1dccbc
jones1913 <jones1913@users.noreply.github.com>
2017-01-29 11:49:02 +0100

    * add tebasuna51's extension additions and fixes * mark some encoders as deprecated * fix compile.bat again (force x86 build)

--- compile2010.bat
+++ compile2010.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 pushd "%~dp0"
 rmdir /S /Q dist\
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe BeHappyVS2010.csproj /t:Rebuild /p:Configuration=Release /p:Platform=x86 /p:OutputPath=.\dist
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe BeHappyVS2010.csproj /t:Rebuild /p:Configuration=Release /p:PlatformTarget=x86 /p:OutputPath=.\dist
 xcopy /S extensions\*.* dist\extensions\*.*
 copy LICENSE.txt dist\
 
---
+++ extensions/deprecated/Readme.txt
@@ -0,0 +1,4 @@
+The encoder extensions here are either deprecated, useless or not recommened to use for various reasons.
+
+If you need to use them just copy them up to '../extensions/*' .
+
