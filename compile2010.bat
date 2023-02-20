9c3fa9a6d3f67b71a8f434376e184256b460588f
jones1913 <jones1913@users.noreply.github.com>
2023-02-20 20:04:46 +0100

    update vs project files, enable x64 build

--- compile2010.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-pushd "%~dp0"
-rmdir /S /Q dist\
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe BeHappyVS2010.csproj /t:Rebuild /p:Configuration=Release /p:PlatformTarget=x86 /p:OutputPath=.\dist
-xcopy /S extensions\*.* dist\extensions\*.*
-copy LICENSE.txt dist\
-
-PAUSE
