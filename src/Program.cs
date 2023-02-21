34b274e825f7d5e9731c8bca5111a50ba8603ee1
jones1913 <jones1913@users.noreply.github.com>
2023-02-21 18:20:03 +0100

    remove platform check at runtime

--- src/Program.cs
+++ src/Program.cs
@@ -16,18 +16,6 @@ namespace BeHappy
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			// To avoid ugly BadImageFormatException let's check current mode
-			int pointerSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(IntPtr));
-			if (pointerSize != 4)
-			{
-				// Can't run in non-32-bit mode!
-				int processBits = pointerSize * 8;
-				string text = string.Format("BeHappy is designed to run as 32-bit process.\nUnfortunally this build (v{1}) is invalid and started as {0}-bit process. Please inform developers.\nBeHappy will be aborted.", processBits, Application.ProductVersion);
-				string caption = string.Format("Can't run as {0}-bit process!", processBits);
-				MessageBox.Show(text, caption, MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
-				return;
-			}
-			
 			Application.Run(new MainForm());
 		}
 	}
